8c876f8180a662f63883d6bd60c9c93ed2bba71d
kevigira50 <kevigira50@gmail>
2021-12-06 11:09:38 +0200

    'adding'

--- ussd/views.py
+++ ussd/views.py
@@ -24,16 +24,24 @@ def ussdApp(request):
         category = text[:3]
         response =""
         if text == '':
-            response =  "CON Murakaza neza kuri Idafarm \n"
-            response += "1. Kwandikisha igihingwa \n"
-            response += "2. Kumenya ingengabihe \n"
-            response += "3. Umushahara"
+            response =  "CON you are welcome to Easy Go! \n"
+            response += "1. press 1 to continue in english \n"
+            response += "2. kanda 2 gukomeza mu kinyarwanda \n"
+            response += "3. Gusaba ubufasha \n "
         elif text == '1':
 
-            response = "CON Hitamo igihingwa \n"
+            response = "CON Where are you heading? \n"
+            response +="1.Nyagatare \n"
+            response +="Ruhango \n"
+            response +="Nyanza \n"
             response += "00. SUbira inyuma \n"
+        elif text == '1'
+            response = "Here are agencies that do tour to Nyagatare"
+            response +="Horizon\n"
+            response +="Stella\n"
         elif text == '1*00':
-                text = level-1   
+                te
+                xt = level-1
 
         else:
             response = "END Ukanze ibitaribyo, ongera mukanya"
