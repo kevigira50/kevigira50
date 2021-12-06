9706f735624571734cd48e77f085cb75fb9909a1
kevigira50 <kevigira50@gmail>
2021-12-06 11:31:26 +0200

    'adds'

--- ussd/views.py
+++ ussd/views.py
@@ -22,26 +22,25 @@ def ussdApp(request):
         text = request.POST['text']
         level = text.split('*')
         category = text[:3]
-        response =""
+        response = ""
         if text == '':
-            response =  "CON you are welcome to Easy Go! \n"
+            response = "CON you are welcome to Easy Go! \n"
             response += "1. press 1 to continue in english \n"
             response += "2. kanda 2 gukomeza mu kinyarwanda \n"
             response += "3. Gusaba ubufasha \n "
         elif text == '1':
 
             response = "CON Where are you heading? \n"
-            response +="1.Nyagatare \n"
-            response +="Ruhango \n"
-            response +="Nyanza \n"
+            response +="1. Nyagatare \n"
+            response +="2. Ruhango \n"
+            response +="3.Nyanza \n"
             response += "00. SUbira inyuma \n"
-        elif text == '1'
-            response = "Here are agencies that do tour to Nyagatare"
-            response +="Horizon\n"
-            response +="Stella\n"
+        elif text == '1':
+            response = "CON Here are agencies that do tour to Nyagatare\n"
+            response +="1.Horizon\n"
+            response +="2.Stella\n"
         elif text == '1*00':
-                te
-                xt = level-1
+             text = level-1
 
         else:
             response = "END Ukanze ibitaribyo, ongera mukanya"
